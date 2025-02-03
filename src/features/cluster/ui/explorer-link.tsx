d22e6fac10e77066a89d7afb14fae8054e40f190
Bram Borggreve <beeman@beeman.nl>
2025-02-03 20:16:59 +0000

    feat: add explorer links and refresh tokens button

--- src/features/cluster/ui/explorer-link.tsx
+++ src/features/cluster/ui/explorer-link.tsx
@@ -1,4 +1,5 @@
-import { Anchor, AnchorProps } from '@mantine/core'
+import { LucideExternalLink } from 'lucide-react'
+import { ActionIcon, ActionIconProps, Anchor, AnchorProps, Tooltip } from '@mantine/core'
 import { useCluster } from '@/features/cluster/data-access/cluster-provider'
 
 export function ExplorerLink({ path, label, ...props }: { path: string; label: string } & AnchorProps) {
@@ -7,5 +8,24 @@ export function ExplorerLink({ path, label, ...props }: { path: string; label: s
     <Anchor href={getExplorerUrl(path)} target="_blank" rel="noopener noreferrer" {...props}>
       {label}
     </Anchor>
+  )
+}
+
+export function ExplorerIcon({ path, ...props }: { path: string } & ActionIconProps) {
+  const { getExplorerUrl } = useCluster()
+  return (
+    <Tooltip label="View on Solana Explorer">
+      <ActionIcon
+        size="xs"
+        variant="light"
+        component="a"
+        href={getExplorerUrl(path)}
+        target="_blank"
+        rel="noopener noreferrer"
+        {...props}
+      >
+        <LucideExternalLink size={12} />
+      </ActionIcon>
+    </Tooltip>
   )
 }
--- src/features/member-dashboard/ui/member-ui-token-list.tsx
+++ src/features/member-dashboard/ui/member-ui-token-list.tsx
@@ -1,14 +1,26 @@
 import { useMemo } from 'react'
-import { Avatar, Box, Card, Flex, Group, SimpleGrid, Skeleton, Text } from '@mantine/core'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { PublicKey } from '@solana/web3.js'
+import { LucideRefreshCw } from 'lucide-react'
+import { ActionIcon, Avatar, Box, Card, Flex, Group, SimpleGrid, Skeleton, Text, Tooltip } from '@mantine/core'
 import { AppToken, tokenSOL } from '@/app-config'
 import { useAppConfig } from '@/app-config-provider'
 import { useBalance, useTokenAccounts } from '@/features/account/data-access/use-token-accounts'
+import { ExplorerIcon } from '@/features/cluster/ui'
 import classes from './member-ui-token-list.module.css'
 
 export function MemberUiTokenList() {
   const { tokens } = useAppConfig()
+  const { publicKey } = useWallet()
   const queryTokenAccounts = useTokenAccounts()
   const queryBalance = useBalance()
+
+  const accounts = useMemo(() => {
+    if (!queryTokenAccounts.data) {
+      return []
+    }
+    return queryTokenAccounts.data
+  }, [queryTokenAccounts.data])
 
   const amount = useMemo(() => {
     if (!queryTokenAccounts.data) {
@@ -29,12 +41,32 @@ export function MemberUiTokenList() {
     <Card withBorder radius="md" className={classes.card}>
       <Group justify="space-between">
         <Text fw={700}>Tokens</Text>
+        <Tooltip label={isLoading ? 'Refreshing...' : 'Refresh balances'}>
+          <ActionIcon
+            loading={isLoading}
+            size="xs"
+            variant="light"
+            onClick={() => Promise.all([queryTokenAccounts.refetch(), queryBalance.refetch()])}
+          >
+            <LucideRefreshCw size={12} />
+          </ActionIcon>
+        </Tooltip>
       </Group>
       <Skeleton visible={isLoading} radius="md" animate={false}>
         <SimpleGrid cols={1} mt="md">
-          <TokenItem amount={queryBalance.data?.toString() || '0'} token={tokenSOL} decimal={9} />
+          <TokenItem
+            amount={queryBalance.data?.toString() || '0'}
+            token={tokenSOL}
+            decimal={9}
+            pubkey={publicKey ?? undefined}
+          />
           {tokens.map((item) => (
-            <TokenItem key={item.address} amount={amount[item.address] || '0'} token={item} />
+            <TokenItem
+              key={item.address}
+              amount={amount[item.address] || '0'}
+              token={item}
+              pubkey={accounts.find((a) => a.account.data.parsed.info.mint === item.address)?.pubkey ?? undefined}
+            />
           ))}
         </SimpleGrid>
       </Skeleton>
@@ -42,18 +74,31 @@ export function MemberUiTokenList() {
   )
 }
 
-function TokenItem({ amount, decimal = 0, token }: { amount: string; decimal?: number; token: AppToken }) {
+function TokenItem({
+  pubkey,
+  amount,
+  decimal = 0,
+  token,
+}: {
+  pubkey?: PublicKey
+  amount: string
+  decimal?: number
+  token: AppToken
+}) {
   return (
     <Flex key={token.address} className={classes.item} p="md" align="center" gap="md">
       <Avatar src={token.image} size={48} />
-      <Box>
-        <Text size="md" fw={700}>
-          {token.name}
-        </Text>
-        <Text size="sm" c="dimmed" fw={400}>
-          {formatTokenAmount(Number(amount) / 10 ** decimal)} {token.symbol}
-        </Text>
-      </Box>
+      <Group justify="space-between" style={{ flex: 1 }}>
+        <Box>
+          <Text size="md" fw={700}>
+            {token.name}
+          </Text>
+          <Text size="sm" c="dimmed" fw={400}>
+            {formatTokenAmount(Number(amount) / 10 ** decimal)} {token.symbol}
+          </Text>
+        </Box>
+        {pubkey ? <ExplorerIcon path={`account/${pubkey}`} /> : null}
+      </Group>
     </Flex>
   )
 }
