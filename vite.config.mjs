728b481aebfc916f3606801dff0dac7eb410b2f3
Bram Borggreve <beeman@beeman.nl>
2025-02-03 14:09:27 +0000

    fix: build config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { defineConfig } from 'vite';
 import { analyzer } from 'vite-bundle-analyzer';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [analyzer({ analyzerMode: 'static' }), nodePolyfills({ exclude: ['fs'] }), react(), tsconfigPaths()],
+  plugins: [
+    analyzer({ analyzerMode: 'static' }),
+    nodePolyfills({
+      overrides: {
+        // Since `fs` is not supported in browsers, we can use the `memfs` package to polyfill it.
+        fs: 'memfs',
+      },
+    }),
+    react(),
+    tsconfigPaths(),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
-  },
-  build: {
-    rollupOptions: {
-      external: [
-        '@metaplex-foundation/umi-eddsa-web3js',
-      ],
-    },
   },
 });
